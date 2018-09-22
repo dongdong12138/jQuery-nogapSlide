6ab9c29eb937299b9caf4e897ae8825179b7c41d
dongdong <286084845@qq.com>
2018-09-22 23:44:34 +0800

    无缝轮播3.0

--- main.js
+++ main.js
@@ -1,36 +1,50 @@
 let n
+let size = $('.images > img').length 
+$('.info > span:last-child').text(size)
 reset()
 
-setInterval(() => {
-    
-    makeLeave(getImage(n)).one('transitionend', (event) => {
-        makeEnter($(event.currentTarget))
-    })
-    makeCurrent(getImage(n+1))
-    n ++
-}, 2000)
+var timeId = setTimer()
+$('.window').on('mouseenter', () => {
+    window.clearInterval(timeId)
+})
+$('.window').on('mouseleave', () => {
+    timeId = setTimer()
+})
 
+
+
+
+
+// 封装函数
+function setTimer () {
+    return setInterval(() => {
+        makeLeave(getImage(n)).one('transitionend', (event) => {
+            makeEnter($(event.currentTarget))
+        })
+        makeCurrent(getImage(n+1))
+        n ++
+        
+    }, 2000)
+}
 function getImage (n) {
     return $(`.images > img:nth-child(${x(n)})`)
 }
-
 function x (n) {
-    if (n > 5) {
-        n = n % 5
+    if (n > size) {
+        n = n % size
         if (n === 0) {
-            n = 5
+            n = size
         }
     }
     return n
 }
-
 function reset () {
     n = 1
     $(`.images > img:nth-child(${n}`).addClass('current').siblings().addClass('enter')
 }
-
 function makeCurrent ($node) {
     $node.removeClass('enter leave').addClass('current')
+    $('.info > span:first-child').text( $node.index()+1 )
     return $node
 }
 function makeLeave ($node) {
